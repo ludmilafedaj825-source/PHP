ed5526cdc6c3fdd93fa68aa31b700303279ab516
ludmilafedaj825-source <ludmilafedaj825@gmail.com>
2026-09-15 16:33:25 +0300

    Lab2

--- index.php
+++ index.php
@@ -1,4 +1,23 @@
 <?php
-echo "<h1>Hello, World! Це моя перша PHP-програма.</h1>";
-phpinfo();
+$appName = "Task Manager";
+$taskTitle = "Вивчити PHP";
+$taskTimeEstimate = 10;
 ?>
+<!DOCTYPE html>
+<html lang="uk">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+<main>
+    <header>
+        <h1><?= $appName; ?></h1>
+        <ul>
+            <li> <?= $taskTitle; ?></li>
+            <li> <?= $taskTimeEstimate ;?> </li>
+        </ul>
+    </header>
+</main>
+</body>
+</html>
