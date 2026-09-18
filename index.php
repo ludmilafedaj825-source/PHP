41a574da0bc27b915bfa5adc6767739b47ae81d3
ludmilafedaj825-source <ludmilafedaj825@gmail.com>
2026-09-18 16:46:30 +0300

    Update. Loops added

--- index.php
+++ index.php
@@ -2,12 +2,23 @@
 $appName = "Task Manager";
 $taskTitle = "Вивчити PHP";
 $taskTimeEstimate = 10;
+$isCompleted = false;
 ?>
 <!DOCTYPE html>
 <html lang="uk">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        .task-done{
+            color: green;
+            text-decoration:underline;
+        }
+        .task-pending{
+            color: gray;
+            text-decoration: underline;
+        }
+    </style>
 </head>
 <body>
 <main>
@@ -15,6 +26,13 @@ $taskTimeEstimate = 10;
         <h1><?= $appName; ?></h1>
         <ul>
             <li> <?= $taskTitle; ?></li>
+            <li class="<?= $isCompleted ? 'task-done' : 'task-pending' ?>">
+                <?php if ($isCompleted): ?>
+                    <p> ✔️ Виконано </p>
+                <?php else: ?>
+                    <p> 🕒 В процесі </p>
+                <?php endif; ?>
+            </li>
             <li> <?= $taskTimeEstimate ;?> </li>
         </ul>
     </header>
