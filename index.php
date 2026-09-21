f7be394816accbc20e77b3bdf76ca897303f38e8
Lusy_Fediai <ludmilafedaj825@gmail.com>
2026-09-21 21:39:22 +0300

    Added

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+$appName = "Task Manager";
+$taskTitle = "Вивчити PHP";
+$taskTimeEstimate = 10;
+$isCompleted = false;
+?>
+<!DOCTYPE html>
+<html lang="uk">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        .task-done{
+            color: green;
+            text-decoration:underline;
+        }
+        .task-pending{
+            color: gray;
+            text-decoration: underline;
+        }
+    </style>
+</head>
+<body>
+<main>
+    <header>
+        <h1><?= $appName; ?></h1>
+        <ul>
+            <li> <?= $taskTitle; ?></li>
+            <li class="<?= $isCompleted ? 'task-done' : 'task-pending' ?>">
+                <?php if ($isCompleted): ?>
+                    <p> ✔️ Виконано </p>
+                <?php else: ?>
+                    <p> 🕒 У процесі </p>
+                <?php endif; ?>
+            </li>
+            <li> <?= $taskTimeEstimate ;?> </li>
+        </ul>
+    </header>
+</main>
+</body>
+</html>
